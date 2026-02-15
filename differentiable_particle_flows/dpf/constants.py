aab0da2407f80d20be3580a2f4bf33197b687a6b
Chunchao <chunchaoma@hotmail.com>
2026-02-15 21:38:53 +0000

    add seed in constants files

--- differentiable_particle_flows/dpf/constants.py
+++ differentiable_particle_flows/dpf/constants.py
@@ -6,3 +6,4 @@ import tensorflow as tf
 
 DEFAULT_DTYPE = tf.float64      # Use float64 for numerical precision
 DEFAULT_ESS_THRESHOLD = 0.5     # Resample when ESS < threshold * N
+DEFAULT_SEED = 0                # Default random seed for reproducibility
