77e28e28b4ffa2a695b4cfa10891c4934c4dd608
Chunchao <chunchaoma@hotmail.com>
2026-02-15 20:15:37 +0000

    add shared constants

---
+++ differentiable_particle_flows/dpf/constants.py
@@ -0,0 +1,8 @@
+"""
+Shared constants for the standard particle filter.
+"""
+
+import tensorflow as tf
+
+DEFAULT_DTYPE = tf.float64      # Use float64 for numerical precision
+DEFAULT_ESS_THRESHOLD = 0.5     # Resample when ESS < threshold * N
